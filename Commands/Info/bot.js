45c8dbb02dcf3f615140569e351a6c605cc2ad66
NeckitWin <neckit.v@gmail.com>
2023-12-23 21:06:11 +0100

    add package json

---
+++ Commands/Info/bot.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+console.log("user.js загружен✅");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('bot')
+        .setDescription('Показывает информацию о боте'),
+    async execute(interaction) {
+        const embed = {
+            color: 0x0099ff,
+            title: `Информация о боте ${interaction.client.user.username}`,
+            thumbnail: {
+                url: interaction.client.user.displayAvatarURL({ dynamic: true }),
+            },
+            fields: [
+                {
+                    name: 'Меня зовут Элэйн',
+                    value: '',
+                    inline: true,
+                },
+                {
+                    name: 'Моё ID',
+                    value: interaction.client.user.id,
+                    inline: false,
+                },
+                {
+                    name: 'Когда я была создана',
+                    value: interaction.client.user.createdAt.substr(0, 9),
+                    inline: true,
+                }
+            ],
+        };
+        interaction.reply({ embeds: [embed] });
+    }
+};
