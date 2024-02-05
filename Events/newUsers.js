f2c9e9cb66d63c1ec8b0454a5c6542a04efe77d5
NeckitWin <neckit.v@gmail.com>
2024-02-05 22:01:52 +0100

    fix crash due to newUsers

--- Events/newUsers.js
+++ Events/newUsers.js
@@ -1,4 +1,5 @@
-const { Events, MessageEmbed } = require('discord.js');
+// Message for new users in system channel
+const { Events, MessageEmbed, Permissions } = require('discord.js');
 const { greetings } = require('../Data/greetings.json');
 
 console.log("Events/newUsers loaded✅")
@@ -6,28 +7,32 @@ console.log("Events/newUsers loaded✅")
 module.exports = {
     name: Events.GuildMemberAdd,
     async execute(member) {
-        randomGreeting = greetings[Math.floor(Math.random() * (greetings.length-1))];
+        const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
 
         try {
             const systemChannel = member.guild.systemChannel;
-            console.log("get channel")
-            if (systemChannel) {
-                const embed = {
-                    color: 65407,
-                    title: 'Добро пожаловать на наш сервер!',
-                    description: `Привет, ${member}! ознакомьтесь с правилами! \n И приступайте к общению!`,
-                    thumbnail: {
-                        url: member.user.displayAvatarURL({ dynamic: true }),
-                    },
-                    image: {
-                        url: randomGreeting,
-                    },
-                    timestamp: new Date(),
-                };
 
-                systemChannel.send({  embeds: [embed] , content: `||${member}||`});
+            if (systemChannel){
+                if (systemChannel.permissionsFor(member.client.user).has("SendMessages")) {
+                    const embed = {
+                        color: 65407,
+                        title: 'Добро пожаловать на наш сервер!',
+                        description: `Привет, ${member}! ознакомьтесь с правилами! \n И приступайте к общению!`,
+                        thumbnail: {
+                            url: member.user.displayAvatarURL({dynamic: true}),
+                        },
+                        image: {
+                            url: randomGreeting,
+                        },
+                        timestamp: new Date(),
+                    };
+
+                    systemChannel.send({embeds: [embed], content: `||${member}||`});
+                }else{
+                    console.log("У меня нет прав на отправку сообщений в системный канал");
+                }
             } else {
-                console.log("Не найдено системного канала");
+                console.log("Не найдено системного канала или нет прав на отправку сообщений");
             }
         } catch (error) {
             console.error(error);
