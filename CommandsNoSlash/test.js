619a03c694713148f6a2282f441bd29a65570e56
NeckitWin <neckit.v@gmail.com>
2024-02-23 01:01:07 +0100

    add locate answer depence language public server

--- CommandsNoSlash/test.js
+++ CommandsNoSlash/test.js
@@ -1,13 +1,25 @@
 const {CommandInteraction, AttachmentBuilder, EmbedBuilder} = require('discord.js');
-const { greetings } = require('../Data/greetings.json');
+const en = require('../Data/Lang/en.json');
+const ru = require('../Data/Lang/ru.json');
+const uk = require('../Data/Lang/uk.json');
+const pl = require('../Data/Lang/pl.json');
+const lang = {ru, en, uk, pl};
 
 module.exports = {
     name: 'test',
     description: 'Тестовая команда',
     async execute(message) {
         try {
-            message.reply('Тестовая команда');
-        }catch (error) {
+            const preferredLang = message.guild.preferredLocale;
+
+            // Проверка на доступность языка
+            if (!lang.hasOwnProperty(preferredLang)) {
+                message.reply(lang.en.test);
+                return;
+            }
+
+            message.reply(lang[preferredLang].test);
+        } catch (error) {
             console.error('Ошибка при использовании команды test:', error);
             console.log('Не удалось обработать запрос.');
         }
