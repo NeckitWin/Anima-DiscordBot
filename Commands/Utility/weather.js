e7560be0ebd7f4e361ad028c6b6490ac5769a287
NeckitWin <neckit.v@gmail.com>
2024-03-06 20:35:38 +0100

    add localization to other package

--- Commands/Utility/weather.js
+++ Commands/Utility/weather.js
@@ -1,10 +1,6 @@
 const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 const axios = require('axios');
-const en = require('../../Data/Lang/en.json');
-const ru = require('../../Data/Lang/ru.json');
-const uk = require('../../Data/Lang/uk.json');
-const pl = require('../../Data/Lang/pl.json');
-const lang = {ru, en, uk, pl};
+const lang = require('../../Data/Lang/lang');
 
 console.log("command weather loaded");
 
@@ -29,6 +25,7 @@ module.exports = {
             })
             .setRequired(true)),
     async execute(interaction) {
+        console.log(lang);
         try {
             let preferredLang = interaction.guild.preferredLocale;
             if (!lang.hasOwnProperty(preferredLang)) {
---
+++ Data/Lang/lang.js
@@ -0,0 +1,7 @@
+const en = require('../../Data/Lang/en.json');
+const ru = require('../../Data/Lang/ru.json');
+const uk = require('../../Data/Lang/uk.json');
+const pl = require('../../Data/Lang/pl.json');
+const lang = {ru, en, uk, pl};
+
+module.exports = lang;
