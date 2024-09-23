d996cc2d74578426fa8c51ad8507ec76e88b640b
NeckitWin <neckit.v@gmail.com>
2024-09-23 21:50:49 +0200

    add command to add data to json

---
+++ CommandsNoSlash/addData.js
@@ -0,0 +1,42 @@
+const {EmbedBuilder} = require('discord.js');
+const fs = require("node:fs");
+const path = require("node:path");
+
+module.exports = {
+    name: 'add_data',
+    description: 'Owner command',
+    async execute(message) {
+        const messageArray = message.content.split(' ');
+        const target = messageArray[1];
+        const newData = messageArray[2];
+
+        const pathFile = path.join(__dirname, "../Data/jsons/reaction.json");
+
+        fs.readFile(pathFile, 'utf8',(err, data)=>{
+            if (err) return console.error(err);
+            let replyMessage;
+
+            try {
+                const jsonData = JSON.parse(data);
+
+                if (jsonData[target]) {
+                    jsonData[target].push(newData);
+                    replyMessage = `success of adding data`;
+                } else {
+                    replyMessage = `json doesn't has this target`;
+                }
+
+                fs.writeFile(pathFile, JSON.stringify(jsonData, null, 2), err=>{
+                    if (err) {
+                        console.error(err);
+                        replyMessage=`json write has error`;
+                    }
+                })
+
+                message.reply(replyMessage);
+            } catch (e) {
+                console.error(e)
+            }
+        })
+    }
+}
