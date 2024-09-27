948a76a231dd7e79e3a0266549dbb075234e6f16
NeckitWin <neckit.v@gmail.com>
2024-09-27 13:57:43 +0200

    fix command say

--- Commands/Admin/say.js
+++ Commands/Admin/say.js
@@ -1,9 +1,10 @@
-const {SlashCommandBuilder} = require('discord.js');
+const {SlashCommandBuilder, PermissionFlagsBits} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('say')
         .setDescription('Send by bot')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .addStringOption(option =>
             option.setName('text')
                 .setDescription('the text you want to send')
@@ -11,6 +12,7 @@ module.exports = {
         ),
     async execute(interaction) {
         const target = interaction.options.getString('text');
-        interaction.reply({content: target});
+        await interaction.reply({ content: `Sending your message...`, ephemeral: true });
+        await interaction.channel.send({content: target});
     }
 }
