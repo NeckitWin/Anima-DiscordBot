683dc1ef53a6bd4ddd82361b896e99c84cc380f5
NeckitWin <neckit.v@gmail.com>
2024-10-01 21:01:27 +0200

    add event greeting

---
+++ Events/greeting.js
@@ -0,0 +1,29 @@
+const {Events, EmbedBuilder} = require(`discord.js`);
+const path = require("node:path");
+const fs = require("node:fs");
+const greetingsGif = require("../Data/jsons/greeting_gif.json");
+
+module.exports = {
+    name: Events.GuildMemberAdd,
+    async execute(member) {
+        const pathFile = path.join(__dirname, "../Data/jsons/greeting.json");
+        const data = await fs.promises.readFile(pathFile, 'utf8');
+        const jsonData = JSON.parse(data);
+        const serverID = member.guild.id;
+
+        const foundServer = jsonData.find(el => el.server === serverID);
+        if (!foundServer) return;
+        const greetingChannel = member.guild.channels.cache.get(foundServer.channel);
+
+        const randomGif = parseInt(Math.random()*greetingsGif.length);
+        const gifGreeting = greetingsGif[randomGif];
+
+        const embed = new EmbedBuilder()
+            .setTitle(`Welcome to the server!`)
+            .setDescription(`Please familiarize yourself with the rules and continue chatting`)
+            .setColor(`#a493ff`)
+            .setThumbnail(member.user.avatarURL())
+            .setImage(foundServer.picture !== "" ? foundServer.picture : gifGreeting)
+        await greetingChannel.send({content: `${member}`, embeds:[embed]})
+    }
+}
