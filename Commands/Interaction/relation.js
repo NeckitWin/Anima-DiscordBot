8a4fadf3f7a976e88777b4ebb88e7e253e28cc17
NeckitWin <neckit.v@gmail.com>
2024-10-28 03:38:27 +0100

    add try catch

--- Commands/Interaction/relation.js
+++ Commands/Interaction/relation.js
@@ -22,46 +22,49 @@ module.exports = {
                 })
                 .setRequired(false)),
     async execute(interaction) {
-        if (interaction.user.id != `429562004399980546`) return interaction.reply(`In development | В разработке | W trakcie opracowywania | В розробці`);
-        const mentionUser = interaction.options.getUser(`user`);
-        if (mentionUser.bot) return interaction.reply(`Вы не можете начать отношения с ботом`);
+        try {
+            if (interaction.user.id != `429562004399980546`) return interaction.reply(`In development | В разработке | W trakcie opracowywania | В розробці`);
+            const mentionUser = interaction.options.getUser(`user`);
+            if (mentionUser.bot) return interaction.reply(`Вы не можете начать отношения с ботом`);
 
-        const embedError = new EmbedBuilder()
-            .setColor(`#c30000`)
+            const embedError = new EmbedBuilder()
+                .setColor(`#c30000`)
 
-        if (mentionUser) {
-            if (mentionUser.id === interaction.user.id) {
-                embedError.setTitle(`Вы не можете начать отношения с собой`);
-                return interaction.reply({embeds: [embedError]});
+            if (mentionUser) {
+                if (mentionUser.id === interaction.user.id) {
+                    embedError.setTitle(`Вы не можете начать отношения с собой`);
+                    return interaction.reply({embeds: [embedError]});
+                }
+
+                const embed = new EmbedBuilder()
+                    .setTitle(`Свадьба`)
+                    .setDescription(`${interaction.user.displayName} сделал вам предложение руки и сердца`)
+
+                const ButtonAccept = new ButtonBuilder()
+                    .setLabel(`Принять`)
+                    .setStyle(`Success`)
+                    .setCustomId(`acceptRelation`);
+
+                const ButtonDecline = new ButtonBuilder()
+                    .setLabel(`Отклонить`)
+                    .setStyle(`Primary`)
+                    .setCustomId(`declineRelation`);
+
+                const ActionRow = new ActionRowBuilder()
+                    .addComponents(ButtonAccept, ButtonDecline);
+
+                interaction.reply({content: `${mentionUser}`, embeds: [embed], components: [ActionRow]});
+            } else {
+                const checkRelation = await getRelation(interaction.guild.id, interaction.user.id);
+                if (!checkRelation.length > 0) return interaction.reply(`Вы не в отношениях`);
+                const relation = checkRelation[0];
+                const embed = new EmbedBuilder()
+
+                console.log(relation)
+                interaction.reply(`test`);
             }
-
-            const embed = new EmbedBuilder()
-                .setTitle(`Свадьба`)
-                .setDescription(`${interaction.user.displayName} сделал вам предложение руки и сердца`)
-
-            const ButtonAccept = new ButtonBuilder()
-                .setLabel(`Принять`)
-                .setStyle(`Success`)
-                .setCustomId(`acceptRelation`);
-
-            const ButtonDecline = new ButtonBuilder()
-                .setLabel(`Отклонить`)
-                .setStyle(`Primary`)
-                .setCustomId(`declineRelation`);
-
-            const ActionRow = new ActionRowBuilder()
-                .addComponents(ButtonAccept, ButtonDecline);
-
-            interaction.reply({content: `${mentionUser}`, embeds: [embed], components: [ActionRow]});
-        } else {
-            const checkRelation = await getRelation(interaction.guild.id, interaction.user.id);
-            if (!checkRelation.length > 0) return interaction.reply(`Вы не в отношениях`);
-            const relation = checkRelation[0];
-            const embed = new EmbedBuilder()
-
-            console.log(relation)
-            interaction.reply(`test`);
+        } catch (err) {
+            console.error(err);
         }
-
     }
 }
--- Events/relation.js
+++ Events/relation.js
@@ -7,31 +7,34 @@ module.exports = [
         async execute(interaction) {
             if (!interaction.isButton()) return;
             if (!['declineRelation', 'acceptRelation', 'cancelRelation'].includes(interaction.customId)) return;
+            try {
+                const guildID = interaction.guild.id;
+                const authorInteraction = interaction.message.interaction.user;
+                const user = interaction.user;
+                const mentionUser = interaction.message.mentions.users.first();
+                const embedError = new EmbedBuilder()
+                    .setColor(`#c30000`);
 
-            const guildID = interaction.guild.id;
-            const authorInteraction = interaction.message.interaction.user;
-            const user = interaction.user;
-            const mentionUser = interaction.message.mentions.users.first();
-            const embedError = new EmbedBuilder()
-                .setColor(`#c30000`);
+                if (authorInteraction.id === user.id) return interaction.reply({
+                    content: `Вы не можете начать отношения с собой`,
+                    ephemeral: true
+                });
+                if (mentionUser.id !== user.id) return interaction.reply({
+                    content: `Это предложение было сделано не вам`,
+                    ephemeral: true
+                });
+                if (interaction.customId === `acceptRelation`) {
 
-            if (authorInteraction.id === user.id) return interaction.reply({
-                content: `Вы не можете начать отношения с собой`,
-                ephemeral: true
-            });
-            if (mentionUser.id !== user.id) return interaction.reply({
-                content: `Это предложение было сделано не вам`,
-                ephemeral: true
-            });
-            if (interaction.customId === `acceptRelation`) {
-
-                const tryRelation = await setRelation(interaction.guild.id, authorInteraction.id, mentionUser.id);
-                if (tryRelation) {
-                    await interaction.reply({content: `Вы успешно начали отношения`});
-                } else {
-                    embedError.setTitle(`Вы уже в отношениях`);
-                    await interaction.reply({embeds: [embedError]});
+                    const tryRelation = await setRelation(interaction.guild.id, authorInteraction.id, mentionUser.id);
+                    if (tryRelation) {
+                        await interaction.reply({content: `Вы успешно начали отношения`});
+                    } else {
+                        embedError.setTitle(`Вы уже в отношениях`);
+                        await interaction.reply({embeds: [embedError]});
+                    }
                 }
+            } catch (err) {
+                console.error(err);
             }
         }
     }
