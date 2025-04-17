a5463660bf5c1a3cf524eee0213e8cbd9d12c5b8
NeckitWin <neckit.v@gmail.com>
2025-04-17 17:04:40 +0200

    refactor: help command, add object data

--- Components/Embeds/helpEmbed.js
+++ Components/Embeds/helpEmbed.js
@@ -2,6 +2,14 @@ import {EmbedBuilder} from "discord.js";
 
 export const helpEmbed = (interaction, lang) => {
     const local = lang.menuhelp;
+    const commands = {
+        info: '</help:1285287069676994605>, </bot:1285287069676994604>, </user:1274053573700878358>, </leaders:1287176496791945268>, </avatar-banner:1285287069676994603>, </server:1285287069676994607>, </role:1285287069676994606>',
+        admin: `</language:1292081237837611079>, </autoroles:1329204198402953417>, </welcome set:1329204198402953419>, </welcome preview:1329204198402953419>, </welcome remove:1329204198402953419>, </logs set:1299470943638519890>, </logs remove:1299470943638519890>`,
+        moder: '</clear:1285287069676994609>, </post:1302340870892163093>',
+        util: '</calc:1285287069756690488>, </translate:1285287069756690490>, </weather:1285287069756690491>',
+        games: '</anime:1294302199869997197>, `+aura`, `-aura`',
+        fun: '</rp:1299513629934223493>, </marry:1302061890201063454>, </nsfw:1293672483869949973>, </ship:1295137310584864868>, </ben:1292452492986155051>, </ball:1300172412628762656>'
+    }
     return new EmbedBuilder()
         .setTitle(local.placeholder)
         .setThumbnail(interaction.client.user.displayAvatarURL())
@@ -9,32 +17,32 @@ export const helpEmbed = (interaction, lang) => {
             [
                 {
                     name: `📚・${local.info.label}`,
-                    value: '</help:1285287069676994605>, </bot:1285287069676994604>, </user:1274053573700878358>, </leaders:1287176496791945268>, </avatar-banner:1285287069676994603>, </server:1285287069676994607>, </role:1285287069676994606>',
+                    value: commands.info,
                     inline: false
                 },
                 {
                     name: `👑・${local.admin.label}`,
-                    value: `</language:1292081237837611079>, </autoroles:1329204198402953417>, </welcome set:1329204198402953419>, </welcome preview:1329204198402953419>, </welcome remove:1329204198402953419>, </logs set:1299470943638519890>, </logs remove:1299470943638519890>`,
+                    value: commands.admin,
                     inline: false
                 },
                 {
                     name: `👮‍♂️・${local.moder.label}`,
-                    value: '</clear:1285287069676994609>, </post:1302340870892163093>',
+                    value: commands.moder,
                     inline: false
                 },
                 {
                     name: `🔧・${local.util.label}`,
-                    value: '</calc:1285287069756690488>, </translate:1285287069756690490>, </weather:1285287069756690491>',
+                    value: commands.util,
                     inline: false
                 },
                 {
                     name: `🎮・${local.games.label}`,
-                    value: '</anime:1294302199869997197>, `+aura`, `-aura`',
+                    value: commands.games,
                     inline: false
                 },
                 {
                     name: `🎉・${local.fun.label}`,
-                    value: '</rp:1299513629934223493>, </marry:1302061890201063454>, </nsfw:1293672483869949973>, </ship:1295137310584864868>, </ben:1292452492986155051>, </ball:1300172412628762656>',
+                    value: commands.fun,
                     inline: false
                 }
             ]
